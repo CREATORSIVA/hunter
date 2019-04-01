ab118ea61944c5d35d1e584335adde4172a4159e
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-04-01 15:26:46 +0530

    Create hunter set1 1st.c

---
+++ hunter set1 1st.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   int i,j,len=0,a[80],t=0;
+   for(i=0;i<n;i++)
+   {
+      scanf("%d",&a[i]); 
+   }
+   for(i=0;i<n;i++)
+   {
+       for(j=i+1;j<n;j++)
+       {
+          if(a[j]<a[i])
+          {
+              t=a[j];
+              a[j]=a[i];
+              a[i]=t;
+          }
+       }
+   }
+   for(i=0;i<n;i++)
+   {
+       for(j=i+1;j<n;j++)
+       {
+           if((a[i]==a[j])&&(a[i]!='$'))
+           {
+               printf("%d",a[i]);
+               a[i]='$';
+               a[j]='$';
+           }
+       }
+   }
+
+    return 0;
+}
